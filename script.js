4a1be65b030f3e5a987ac1412c196b593160338c
Namdev23 <nguyennamtiger@gmail.com>
2024-04-21 23:12:17 +0700

    Updated

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyBHxOn0b3FRDBDQZxM-LFYr07GIOXO81sw",
     authDomain: "foricon-database.firebaseapp.com",
     projectId: "foricon-database",
+    databaseURL: "https://foricon-database-default-rtdb.asia-southeast1.firebasedatabase.app",
     storageBucket: "foricon-database.appspot.com",
     messagingSenderId: "895804546140",
     appId: "1:895804546140:web:2e737ccdd006d4e8b7da99",
